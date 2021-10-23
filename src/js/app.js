147df4355e62040e2b9f5d2d670d5838416049c9
libny14623 <14623@utcv.edu.mx>
2021-10-23 09:16:27 -0500

    Actualizacion web manifest mensaje

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ let MAIN;
 let MODAL_POST;
 let BTN_SHOW_POST;
 let BTN_CANCEL_POST;
+let deferredPrompt;
 
 //Funciones
 const showPostModal = () => {
@@ -17,6 +18,23 @@ const showPostModal = () => {
     MODAL_POST.style.transform = 'translateY(100vh)';
   };
 
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log('anulando envio')
+    e.preventDefault();
+    deferredPrompt = e;
+  });
+
+
+  const bannerInstall = document.querySelector('#banner-install');
+    bannerInstall.addEventListener('click', async () => {
+      if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const response = await deferredPrompt.userChoice;
+        if (response.outcome === 'dismissed') {
+          console.error('El usuario cancelo la instalación');
+        }
+      }
+    });
   
 
 // Cuando se cargue todo nuestro DOM
